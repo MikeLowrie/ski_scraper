c0cfa31cd6530e7c1cfe5aeb9cb8e26d11f31876
MikeLowrie <ericmlowrie@gmail.com>
2020-01-24 15:38:49 -0500

    Update skiscraper.py
    Proper print of only the number of open trails on Jack Frost Mountain.

--- skiscraper.py
+++ skiscraper.py
@@ -1,19 +1,20 @@
-print('Ski Scraper Party Time')
-
 from bs4 import BeautifulSoup
 import requests
 
 #electropeak web scraping raspberry pi
 
-r = requests.get("https://www.jfbb.com/snow-report/")
-soup = BeautifulSoup(r.text, 'html.parser')
-result = soup.find_all('div', attrs = {'class':'content-column snow-report-JFBB'})
+def jackfrost():
+  r = requests.get("https://www.jfbb.com/snow-report/")
+  soup = BeautifulSoup(r.text, 'html.parser')
+  result = soup.find_all('div', attrs = {'class':'content-column snow-report-JFBB'})
 
-for new in result:
-        if 'Trails' in new.text:
-                print('Found something', len(new.text))
+  for new in result:
+    if 'Trails' in new.text and len(new.text) < 250:
+      print('Found something', len(new.text))
+      slopes = new.div.contents[1].div.next_sibling.next_sibling.div.string
+      strippedslopes = slopes.strip()
+      print(strippedslopes)
 
-#for new in result:
-#        valuetag = result.find_all('div', attrs = {'class':'value1'})
-        
+print('Ski Scraper Party Time')
+jackfrost()
 print('Done')
