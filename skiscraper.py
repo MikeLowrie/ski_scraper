da238ce0eb3c7dab7f3f59eca3ca1c27730d36ee
MikeLowrie <ericmlowrie@gmail.com>
2020-03-04 13:51:39 -0500

    Added park information to select mountains.

--- skiscraper.py
+++ skiscraper.py
@@ -13,14 +13,38 @@ def jackfrost():
       #print('Found something', len(new.text))
       slopes = new.div.contents[1].div.next_sibling.next_sibling.div.string
       strippedslopes = slopes.strip()
-      print(strippedslopes, 'Trails of 20')
+      print(strippedslopes, 'Trails of 20 open')
     if 'Lifts' in new.text and len(new.text) < 250:
       lifts = new.div.contents[1].div.next_sibling.next_sibling.div.string
       strippedlifts = lifts.strip()
-      print(strippedlifts, 'Lifts of 9')
+      print(strippedlifts, 'Lifts of 9 running')
+    if 'Parks' in new.text and len(new.text) < 250:
+      parks = new.div.contents[1].div.next_sibling.next_sibling.div.string
+      strippedparks = parks.strip()
+      parkinfo = "Park is OPEN"
+      if strippedparks == "0":
+        parkinfo = "Park is CLOSED"
+      print(parkinfo)
 
 def bigboulder():
-  print('No statistics available currently')
+  r = requests.get("https://www.jfbb.com/snow-report/")
+  soup = BeautifulSoup(r.text, 'html.parser')
+  result = soup.find_all('div', attrs = {'class':'content-column snow-report-BBP'})
+
+  for new in result:
+    if 'Trails' in new.text and len(new.text) < 250:
+      #print('Found something', len(new.text))
+      slopes = new.div.contents[1].div.next_sibling.next_sibling.div.string
+      strippedslopes = slopes.strip()
+      print(strippedslopes, 'Trails of 16 open')
+    if 'Lifts' in new.text and len(new.text) < 250:
+      lifts = new.div.contents[1].div.next_sibling.next_sibling.div.string
+      strippedlifts = lifts.strip()
+      print(strippedlifts, 'Lifts of 8 running')
+    if 'Parks' in new.text and len(new.text) < 250:
+      parks = new.div.contents[1].div.next_sibling.next_sibling.div.string
+      strippedparks = parks.strip()
+      print(strippedparks, 'Parks of 8 open')
 
 def bearcreek():
   r = requests.get("https://www.bcmountainresort.com/play/snowsports/pa-ski-area-snowtubing-conditions/")
@@ -33,22 +57,37 @@ def bearcreek():
       #print(check.text, check.next_sibling.string) <-- Removing if statement will show more detailed report
       print(check.next_sibling.string, 'of 23 open')
     if tag == "Open Lifts":
-      print(check.next_sibling.string, 'of 6 open (including snowtubing and beginner carpet)')
+      print(check.next_sibling.string, 'of 6 running')
+    if tag == "Terrain Parks":
+      print(check.next_sibling.string, 'Parks of 3 open')
 
 def camelback():
   r = requests.get("https://www.skicamelback.com/plan-your-trip/snow-report/")
   soup = BeautifulSoup(r.text, 'html.parser')
   dttags = soup.body.find_all('dt')
+  trailinfo = '';
+  liftinfo = '';
   for check in dttags:
     if check.text.strip() == "Open Trails":
       #print(check.text, check.next_sibling.string) <-- Removing if statement will show more detailed report
-      print(check.next_sibling.string, 'of 37')
+      trailinfo = check.next_sibling.string + ' of 39 open';
+    if check.text.strip() == "Open Lifts":
+      liftinfo = check.next_sibling.string + ' of 14 running';
+  print(trailinfo)
+  print(liftinfo)
+  print('Park info not currently present. Please check skicamelback.com directly.')
+
+  #articletags = soup.body.find_all('article', attrs = {'class':'SnowReport-Trail SnotReport-feature'});
+  # Park info is in <article class="SnowReport-Trail SnowReport-feature">
+  # Open/Closed can be checked for the image tag i class="pti pti-open"
 
 def bluemountain():
   r = requests.get("https://www.skibluemt.com/blue-current/")
   soup = BeautifulSoup(r.text, "html.parser")
   spantags = soup.find_all('span', attrs = {'class':'open'})
-  print(spantags[0].string, 'Trails of 40')
+  print(spantags[0].string, 'Trails of 40 open')
+  print(spantags[1].string, 'Lifts of 16 running')
+  print('Park info not currently present. Please check skibluemt.com directly.')
   
 
 print('Lowrie Ski Scraper')
