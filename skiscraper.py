ede247d2ce893c7a41055e8382e70d35af21c194
Eric Lowrie <ericmlowrie@gmail.com>
2022-12-13 11:16:15 -0500

    Update skiscraper.py
    Updates for 2022-2023 season.

--- skiscraper.py
+++ skiscraper.py
@@ -1,48 +1,49 @@
 from bs4 import BeautifulSoup
+from datetime import date
 import requests
 
 #electropeak web scraping raspberry pi
 
 def jackfrost():
-  r = requests.get("https://www.jfbb.com/snow-report/")
+  r = requests.get("https://www.jfbb.com/the-mountain/mountain-conditions/lift-and-terrain-status.aspx")
   soup = BeautifulSoup(r.text, 'html.parser')
-  result = soup.find_all('div', attrs = {'class':'content-column snow-report-JFBB'})
-
-  for new in result:
-    if 'Trails' in new.text and len(new.text) < 250:
-      #print('Found something', len(new.text))
-      slopes = new.div.contents[1].div.next_sibling.next_sibling.div.string
-      strippedslopes = slopes.strip()
-      print(strippedslopes, 'Trails of 20')
+  result = soup.find_all('div', attrs = {'class':'terrain_summary__tab_main__text'})
+  print(result[0].span.contents[0], 'Lifts of 15');
+  print(result[2].span.contents[0], 'Trails of 37')
 
 def bearcreek():
-  r = requests.get("https://www.bcmountainresort.com/play/snowsports/pa-ski-area-snowtubing-conditions/")
+  r = requests.get("https://www.bcmountainresort.com/activities/ski-snowboarding/ski-conditions/")
   soup = BeautifulSoup(r.text, 'html.parser')
-  # Finding the "Open Trails" tag has been cumbersome so instead just yank every <dt> and filter
-  dttags = soup.body.find_all('dt')
-  for check in dttags:
-    if check.text.strip() == "Open Trails":
-      #print(check.text, check.next_sibling.string) <-- Removing if statement will show more detailed report
-      print(check.next_sibling.string, 'of 22')
+  
+  result = soup.find_all('h3')
+  for check in result:
+    if check.text.strip() == "Open trails":
+      print(check.parent.parent.next_sibling.next_sibling.contents[1].p.string, 'of 23')
+    if check.text.strip() == "Open Lifts":
+      print(check.parent.parent.next_sibling.next_sibling.contents[1].p.string, 'of 6 (including Snowtubing)')
 
 def camelback():
-  r = requests.get("https://www.skicamelback.com/plan-your-trip/snow-report/")
+  r = requests.get("https://conditions.camelbackresort.com/conditions/snow-report/snow-report/")
   soup = BeautifulSoup(r.text, 'html.parser')
   dttags = soup.body.find_all('dt')
   for check in dttags:
     if check.text.strip() == "Open Trails":
-      #print(check.text, check.next_sibling.string) <-- Removing if statement will show more detailed report
       print(check.next_sibling.string, 'of 37')
 
 def bluemountain():
-  r = requests.get("https://www.skibluemt.com/blue-current/")
+  r = requests.get("https://snow.skibluemt.com/conditions/snow-report/")
   soup = BeautifulSoup(r.text, "html.parser")
-  spantags = soup.find_all('span', attrs = {'class':'open'})
-  print(spantags[0].string, 'Trails of 40')
+  result = soup.find_all('dt')
+  for check in result:
+    if check.text.strip() == "Open Trails":
+      print(check.next_sibling.string, 'Trails')
+    if check.text.strip() == "Open Lifts":
+      print(check.next_sibling.string, 'Lifts')
   
 
 print('Lowrie Ski Scraper')
-print('----\n', 'Jack Frost:')
+print('Report for', date.today())
+print('----\n', 'Jack Frost and Big Boulder:')
 jackfrost()
 print('----\n', 'Bear Creek:')
 bearcreek()
